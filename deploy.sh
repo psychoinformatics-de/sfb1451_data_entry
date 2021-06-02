15eb8faa445263029530ec735e677598a599c49d
Christian Mönch <christian.moench@web.de>
2021-06-02 13:16:36 +0200

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+git checkout master
+
+mkdir -p www
+
+cp entry.html www
+cp -r css js www
+
+git add www
+git commit -m "pre-deploy commitment"
+
+git checkout deploy
+git merge master -m "deploy merge"
+
+git checkout master
