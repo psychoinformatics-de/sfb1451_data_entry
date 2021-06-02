79df5a52de080630293a034560103e26d3a516de
Christian Mönch <christian.moench@web.de>
2021-06-02 13:06:03 +0200

    clean up html

--- js/verify.js
+++ js/verify.js
@@ -1,3 +1,4 @@
+/*
 'use strict'
 
 
@@ -9,3 +10,26 @@ handednessInput.addEventListener("input", () => {
         handednessInput.setCustomValidity("Nur Lol ist erlaubt");
     }
 })
+*/
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
