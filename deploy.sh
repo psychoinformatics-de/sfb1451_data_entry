eb3cfed8049cdbb80c76670682d4acc69687e2e1
Christian Mönch <christian.moench@web.de>
2021-06-28 10:11:23 +0200

    unify deploy script for test and product

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,42 @@
 
 set -x -e
 
+suffix=""
+if [ "$#" -gt "0" ]; then
+  if [ "$1" == "test" ]; then
+    suffix="-test"
+  fi
+fi
+
+
 git checkout deploy
 
+master-version-file=master-version${suffix}
+touch master-version-file
+new_master=$(git rev-parse master)
+current_master=$(cat master-version-file)
+
+
+if [ "${new_master}" == "${current_master}" ]; then
+  echo "latest master version already deployed"
+  exit 0
+fi
+
+echo "${new_master}" > master-version-file
+git add master-version-file
+
+
 # Generate document root
+html-file=entry${suffix}.html
+
 mkdir -p www
 
-git cat-file blob master:entry.html >www/entry.html
+git cat-file blob master:entry.html >"www/${html-file}"
 cp -r css js www
 
 git add www
-git add www/entry.html
+git add "www/${html-file}"
+
 
 # Generate WSGI directory
 mkdir -p wsgi-scripts
@@ -19,7 +45,7 @@ git cat-file blob master:server/store_data.py >wsgi-scripts/store_data.wsgi
 git add wsgi-scripts
 git add wsgi-scripts/store_data.wsgi
 
-git commit -m "pre-deploy commit"
+git commit -m "pre-deploy${suffix} commit "
 
 git push
 
